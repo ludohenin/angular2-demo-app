904398a6b2e30a9d764622ad9c8c49f5350dcab2
ludohenin <ludovic.henin@yahoo.com>
2015-05-29 18:31:56 +0200

    Refactored

--- gulpfile.js
+++ gulpfile.js
@@ -10,12 +10,13 @@ var liveServer = require('live-server');
 var sourceMaps = require('gulp-sourcemaps');
 
 var PATH = {
-    index: {
-        src: './src/index.html',
-        dest: './dist'
-    },
-    lib: {
-        src: [
+    src: {
+        index: './src/index.html',
+        ts: ['./src/ts/**', './typings/**'],
+        templates: './src/templates/**',
+        css: './src/css/**',
+        flux: './src/flux.js',
+        lib: [
             './node_modules/angular2/node_modules/traceur/bin/traceur-runtime.js',
             './node_modules/angular2/node_modules/zone.js/dist/zone.js',
             './node_modules/es6-module-loader/dist/es6-module-loader-sans-promises.js',
@@ -24,40 +25,57 @@ var PATH = {
             './node_modules/reflect-metadata/Reflect.js.map',
             './node_modules/systemjs/dist/system.js'
         ],
-        dest: './dist/lib'
     },
-    ts: {
-        src: ['./src/ts/**', './typings/**'],
-        dest: './dist/js'
-    },
-    templates: {
-        src: './src/templates/**',
-        dest: './dist/templates',
-    },
-    css: {
-        src: './src/css/**',
-        dest: './dist/css'
+    dest: {
+        index: './dist',
+        ts: './dist/js',
+        templates: './dist/templates',
+        css: './dist/css',
+        flux: './dist/lib',
+        lib: './dist/lib',
     }
-}
+};
+
+// ---------------------------- Main gulp tasks --------------------------------
+
+gulp.task('build', function (done) {
+    runSequence('clean', ['build:lib', 'build:js', 'build:templates', 'build:css', 'build:index'], done);
+});
+
+gulp.task('serve', ['build:js', 'build:templates', 'build:css', 'build:index'], function () {
+    gulp.watch(PATH.src.ts, ['build:js']);
+    gulp.watch(PATH.src.templates, ['build:templates']);
+    gulp.watch(PATH.src.css, ['build:css']);
+    gulp.watch(PATH.src.index, ['build:index']);
+
+    var params = {
+        port: 8080,
+        host: "localhost",
+        root: "./dist",
+        open: false
+    };
+    liveServer.start(params);
+});
+
+// -----------------------------------------------------------------------------
 
 gulp.task('clean:lib', function (done) {
-    del([PATH.lib.dest], done);
+    del([PATH.dest.lib], done);
 });
 
 gulp.task('clean:js', function (done) {
-    del([PATH.ts.dest], done);
+    del([PATH.dest.ts], done);
 });
 
 gulp.task('clean:templates', function (done) {
-    del([PATH.templates.dest], done);
+    del([PATH.dest.templates], done);
 });
 
 gulp.task('clean:css', function (done) {
-    del([PATH.css.dest], done);
+    del([PATH.dest.css], done);
 });
 
 gulp.task('clean', ['clean:lib', 'clean:js', 'clean:templates', 'clean:css']);
-
 
 gulp.task('build:angular2', function () {
     var builder = new Builder({
@@ -71,13 +89,19 @@ gulp.task('build:angular2', function () {
             }
         }
     });
-    builder.build('angular2/router', PATH.lib.dest + '/router.js', {});
-    return builder.build('angular2/angular2', PATH.lib.dest + '/angular2.js', {});
+    builder.build('angular2/router', PATH.dest.lib + '/router.js', {});
+    return builder.build('angular2/angular2', PATH.dest.lib + '/angular2.js', {});
 });
 
-gulp.task('build:lib', ['build:angular2'], function () {
-    gulp.src(PATH.lib.src)
-        .pipe(gulp.dest(PATH.lib.dest));
+// TODO: improve build.
+gulp.task('build:flux', function() {
+    return gulp.src(PATH.src.flux)
+        .pipe(gulp.dest(PATH.dest.flux));
+});
+
+gulp.task('build:lib', ['build:flux', 'build:angular2'], function () {
+    gulp.src(PATH.src.lib)
+        .pipe(gulp.dest(PATH.dest.lib));
 });
 
 var tsProject = ts.createProject('tsconfig.json', {
@@ -85,46 +109,27 @@ var tsProject = ts.createProject('tsconfig.json', {
 });
 
 gulp.task('build:js', ['clean:js'], function () {
-    var result = gulp.src(PATH.ts.src)
+    var result = gulp.src(PATH.src.ts)
         .pipe(plumber())
         .pipe(sourceMaps.init())
         .pipe(ts(tsProject));
 
     return result.js
         .pipe(sourceMaps.write())
-        .pipe(gulp.dest(PATH.ts.dest));
+        .pipe(gulp.dest(PATH.dest.ts));
 });
 
 gulp.task('build:templates', ['clean:templates'], function () {
-    gulp.src(PATH.templates.src)
-        .pipe(gulp.dest(PATH.templates.dest));
+    gulp.src(PATH.src.templates)
+        .pipe(gulp.dest(PATH.dest.templates));
 });
 
 gulp.task('build:css', ['clean:css'], function () {
-    gulp.src(PATH.css.src)
-        .pipe(gulp.dest(PATH.css.dest));
+    gulp.src(PATH.src.css)
+        .pipe(gulp.dest(PATH.dest.css));
 });
 
 gulp.task('build:index', function () {
-    gulp.src(PATH.index.src)
-        .pipe(gulp.dest(PATH.index.dest));
-});
-
-gulp.task('build', function (done) {
-    runSequence('clean', ['build:lib', 'build:js', 'build:templates', 'build:css', 'build:index'], done);
-});
-
-gulp.task('serve', ['build:js', 'build:templates', 'build:css', 'build:index'], function () {
-    gulp.watch(PATH.ts.src, ['build:js']);
-    gulp.watch(PATH.templates.src, ['build:templates']);
-    gulp.watch(PATH.css.src, ['build:css']);
-    gulp.watch(PATH.index.src, ['build:index']);
-
-    var params = {
-        port: 8080,
-        host: "localhost",
-        root: "./dist",
-        open: false
-    };
-    liveServer.start(params);
+    gulp.src(PATH.src.index)
+        .pipe(gulp.dest(PATH.dest.index));
 });
